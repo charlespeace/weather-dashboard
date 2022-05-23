dbea1dcdb14369e3214977d03af2fb4f3fd2845e
Charles Peace <charlespeace@hotmail.com>
2022-05-23 14:16:15 -0700

    add function to render current weather, test api call to make sure data populates in html

--- script.js
+++ script.js
@@ -3,15 +3,52 @@ var weatherApiKey = 'a9dc973acd420ee8d037c44741b54bf0'
 
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
+var weatherContainer = document.querySelector('#weather');
+
+function renderWeather(city, weather) {
+    var tempF = weather.temp;
+    var windMph = weather.wind_speed;
+    var humidity = weather.humidity;
+
+    var card = document.createElement('div');
+    var cardBody = document.createElement('div');
+    var heading = document.createElement('h2');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+
+    card.setAttribute('class', 'card');
+    cardBody.setAttribute('class', 'card-body');
+    card.append(cardBody);
+
+    heading.setAttribute('class', 'h3 card-title');
+    tempEl.setAttribute('class', 'card-text');
+    windEl.setAttribute('class', 'card-text');
+    humidityEl.setAttribute('class', 'card-text');
+
+    heading.textContent = `${city}`;
+    tempEl.textContent = `Temperature: ${tempF}°F`;
+    windEl.textContent = `Wind speed: ${windMph} MPH`;
+    humidityEl.textContent = `Humidity percent: ${humidity} %`;
+    cardBody.append(heading, tempEl, windEl, humidityEl);
+
+    weatherContainer.innerHTML = '';
+    weatherContainer.append(card);
+    
+}
 
 function fetchWeather(location) {
     var { lat } = location;
     var { lon } = location;
+    var city = location.name;
     var apiUrl = `${weatherApiUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}`;
   
     fetch(apiUrl)
     .then(function (res) {
         return res.json();
+    })
+    .then(function(data) {
+        renderWeather(city, data.current)
     })
     .catch(function (err) {
         console.error(err);
@@ -44,7 +81,7 @@ function submitSearchForm(e) {
     }
   
     e.preventDefault();
-    var search = searchInput
+    var search = searchInput.value.trim();
     fetchCoords(search);
     searchInput.value = '';
 }
